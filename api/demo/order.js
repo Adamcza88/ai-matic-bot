29e3cb4188d21c0d53b8464eb9c0fba0b006c819
Adamcza88 <131135605+Adamcza88@users.noreply.github.com>
2025-12-07 14:23:41 +0100

    add  CORS a podporu OPTIONS

--- api/demo/order.js
+++ api/demo/order.js
@@ -3,7 +3,19 @@
 import { createDemoOrder } from "../../server/bybitClient.js";
 import { getUserApiKeys, getUserFromToken } from "../../server/userCredentials.js";
 
+function setCors(res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+}
+
 export default async function handler(req, res) {
+  setCors(res);
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   if (req.method !== "POST") {
     return res.status(405).json({ ok: false, error: "Method not allowed" });
   }
--- api/demo/positions.js
+++ api/demo/positions.js
@@ -1,7 +1,18 @@
 import { getDemoPositions } from "../../server/bybitClient.js";
 import { getUserApiKeys, getUserFromToken } from "../../server/userCredentials.js";
 
+function setCors(res) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+}
+
 export default async function handler(req, res) {
+    setCors(res);
+    if (req.method === "OPTIONS") {
+        res.status(200).end();
+        return;
+    }
     if (req.method !== "GET") {
         res.status(405).json({ ok: false, error: "Method not allowed" });
         return;
